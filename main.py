1f78cdc52231593f6baed1f08d37c04e5f533b36
Bluewizard16 <williamquerry2@gmail.com>
2025-04-07 20:07:19 -0500

    last commit

--- main.py
+++ main.py
@@ -1,49 +1,66 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# import matplotlib
+# matplotlib.use()
+
+
 
 # Buscar en ThingSpeak estaciones meteorológicas:
 # https://thingspeak.mathworks.com/channels/public
 # Ejemplos:
-# https://thingspeak.mathworks.com/channels/870845
 # https://thingspeak.mathworks.com/channels/1293177
 # https://thingspeak.mathworks.com/channels/12397
 
 URLs = [
-    'https://api.thingspeak.com/channels/159150/feeds.csv?results=8000',
-    'https://api.thingspeak.com/channels/196384/feeds.csv?results=8000',
-    'https://api.thingspeak.com/channels/178343/feeds.csv?results=8000',
+  'https://thingspeak.mathworks.com/channels/1293177/feed.csv',
+  'https://thingspeak.mathworks.com/channels/12397/feed.csv'
+  
 ]
-
-
 app = Flask(__name__)
+
 def descargar(url):
-  # descarga el CSV en un dataframe, desde el URL
   df = pd.read_csv(url)
-  # Convertir la cadena en una fecha real
   df['created_at'] = pd.to_datetime(df['created_at'])
-  # Borrar columnas innecesarias
-  if 'field6' in df.columns:
-    df.drop(['entry_id', 'field5', 'field6'], axis=1, inplace=True)
-  else:
-    df.drop(['entry_id', 'field5', 'field6'], axis=1, inplace=True)
-
-  # Renombrar columnas
-  df.columns = ['fecha', 'temp_exterior', 'temp_interior', 'presion_atm', 'humedad']
+  
+  
+  df.drop(['entry_id','field5', 'field6', "field8", "field7"], axis=1, inplace=True)
+  df.columns = ['fecha', 'Temperatura C°', 'Humedad %', 'Pres. Atmosférica (hPa)', 'Gas']
   return df
+    
+def graficar(i,df):
+  lista = []
+  for columna in df.columns[1:]:
+    fig= plt.figure(figsize=(8,5))
+    plt.plot(df['fecha'], df[columna], label=columna)
+    plt.title(f"Historico de {columna} - Estación # {i} ")
+    
+    plt.savefig(f'static/g{i}_{columna}.png')
+    lista.append(f"g{i}_{columna}.png")
+    plt.close()
+  return lista
 
-def graficar(df):
-  # Crear la figura
-  fig = plt.figure(figsize=(8, 5))
-  plt.plot(df['fecha'], df['temp_exterior'], label='Temperatura Exterior')
-  plt.plot(df['fecha'], df['temp_interior'], label='Temperatura Interior')
-
+def actualizar():
+  nombres = []
+  for i,url in  enumerate(URLs):    
+    df  = descargar(url)
+    nombres.extend(graficar(i,df))
+  return nombres
+    
+    
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', nombres = nombres)
 
 # Programa Principal
+
+@app.route('/actualizar')
+def actualizar_datos():
+  global nombres
+  nombres = actualizar()
+  return redirect('/')
 if __name__ == '__main__':   
-
-
+  # Ejecuta la app 
+  nombres = actualizar()
   app.run(host='0.0.0.0', debug=True)
